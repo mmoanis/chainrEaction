7e13493101095b381327873f71ba64659ece1a1b
mmoanis <mohamed_moanis@outlook.com>
2020-02-04 11:00:30 +0100

    Basic endpoints

--- api/index.js
+++ api/index.js
@@ -1,13 +1,60 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+
+import projects from './projects';
 
 const PORT = 5000;
 const app = express();
+// Parse incoming requests data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/api/v1/hello', (req, res) => {
   res.status(200).send({
     success: 'true',
     message: 'hellow trees'
   })
+});
+
+app.get('/api/v1/projects', (req, res) => {
+  res.status(200).send({
+    success: 'true',
+    projects: projects
+  })
+});
+
+app.get('/api/v1/projects/:id', (req, res) => {
+  const id = parseInt(req.params.id, 10);
+  
+  projects.map((project) => {
+    if (project.id === id) {
+      res.status(200).send({
+        success: 'true',
+        project
+      });
+    }
+  });
+
+  return res.status(404).send({
+    success: 'false',
+    message: "No such project!"
+  });
+});
+
+app.post('/api/v1/tree', (req, res) => {
+  if (!req.body.address || !req.body.location) {
+    return res.status(400).send({
+      success: 'false',
+      message: 'Invalid request!'
+    });
+  }
+
+  console.log('Tree added ' + req.body);
+
+  return res.status(201).send({
+    success: 'true',
+    message: 'tree added'
+  });
 });
 
 app.listen(PORT, () => {
---
+++ api/projects.js
@@ -0,0 +1,10 @@
+const projects =  [
+    {
+      id: 1,
+      title: "Toledo",
+      target: "10000",
+      location: ""
+    }
+];
+
+export default projects;
